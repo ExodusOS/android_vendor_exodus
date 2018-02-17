0e0170f9be1ff4385711d9e74e04e54408b45caa
Luca Stefani <luca.stefani.ge1@gmail.com>
2018-02-17 17:50:57 +0100

    bootanimation: Error if screen width or height aren't set
    Change-Id: Ic102e49d39a12116e9a0b25accf0ef81520c495f

--- bootanimation/Android.mk
+++ bootanimation/Android.mk
@@ -46,6 +46,13 @@ ifeq ($(TARGET_BOOTANIMATION),)
     TARGET_BOOTANIMATION := $(TARGET_GENERATED_BOOTANIMATION)
 endif
 
+ifeq ($(TARGET_SCREEN_WIDTH),)
+    $(error TARGET_SCREEN_WIDTH must be set)
+endif
+ifeq ($(TARGET_SCREEN_HEIGHT),)
+    $(error TARGET_SCREEN_HEIGHT must be set)
+endif
+
 include $(CLEAR_VARS)
 LOCAL_MODULE := bootanimation.zip
 LOCAL_MODULE_CLASS := ETC
